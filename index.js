fa276c3dc0ddd8fd8e50a54cd7a5d8647ea16030
Ana Gaita <anafgaita@gmail.com>
2025-07-15 21:32:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const track = document.querySelector('.caroussel-track')
+const slides = Array.from(track.children) /* creates an array with the list elements  - 0-8 */
+const leftBtn = document.querySelector('.caroussel-btn-left')
+const rightBtn = document.querySelector('.caroussel-btn-right')
+
+const slideWidth = slides[0].getBoundingClientRect().width /* why is it used? */
+
+/* arrange the slides next to one another */
+const setSlidePosition = (slide, index) => {
+    slide.style.left = `${slideWidth * index}px`
+}
+slides.forEach(setSlidePosition)
+
+const moveToSlide = (track, currentSlide, targetSlide) => {  /* function to move slides */
+    track.style.transform = `translateX(-${targetSlide.style.left})`
+    currentSlide.classList.remove('current-slide')
+    targetSlide.classList.add('current-slide')
+}
+
+rightBtn.addEventListener('click', e => { /* move to next slide */
+    const currentSlide = track.querySelector('.current-slide')
+    const nextSlide = currentSlide.nextElementSibling
+    
+    moveToSlide(track, currentSlide, nextSlide)
+    
+})
+
+leftBtn.addEventListener('click', e => { /* move to previous slide */
+    const currentSlide = track.querySelector('.current-slide')
+    const previousSlide = currentSlide.previousElementSibling
+
+    moveToSlide(track, currentSlide, previousSlide)
+}) 
